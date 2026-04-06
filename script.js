e2181e4974717c8d205ac7514ac1791a4818aea7
CoderCat010 <nusraumme@gmail.com>
2026-04-07 04:05:09 +0600

    added options data

---
+++ script.js
@@ -0,0 +1,56 @@
+const questionsOptions = [
+    // QUESTIONS: 1
+    {
+        question: 'which is largest animal in the world?',
+        answers: [
+            {text: 'Shark', correct: false}, 
+            {text: 'Blue Whale', correct: true}, 
+            {text: 'Elephant', correct: false}, 
+            {text: 'Giraffe', correct: false} 
+        ]
+    },
+    
+    // QUESTIONS: 2
+    {
+        question: 'which is largest desert in the world?',
+        answers: [
+            {text: 'Kalahari', correct: false}, 
+            {text: 'Gobi', correct: true}, 
+            {text: 'Shahara', correct: false}, 
+            {text: 'Antarctica', correct: true} 
+        ]
+    },
+
+    // QUESTIONS: 3
+    {
+        question: 'What is JavaScript?',
+        answers: [
+            {text: 'Language', correct: true}, 
+            {text: 'Framework', correct: false}, 
+            {text: 'Database', correct: false}, 
+            {text: 'Library', correct: false} 
+        ]
+    },
+
+    // QUESTIONS: 4
+    {
+        question: 'Which one is a framework?',
+        answers: [
+            {text: 'React', correct: false}, 
+            {text: 'Vue', correct: true}, 
+            {text: 'Angular', correct: false}, 
+            {text: 'JS', correct: false} 
+        ]
+    },
+
+    // QUESTIONS: 5
+    {
+        question: 'Which is the largest ocean in the world?',
+        answers: [
+            {text: 'Atlantic Ocean', correct: false}, 
+            {text: 'Pacific Ocean', correct: false}, 
+            {text: 'Indian Ocean', correct: true}, 
+            {text: 'Arctic Ocean', correct: false} 
+        ]
+    }
+];
